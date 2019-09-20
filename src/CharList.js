5371a2451e227899ffabe18a6d3e58731f17b96e
dbriksza <dbriksza@gmail.com>
2019-09-20 14:02:10 -0400

    no search, no mvp

--- src/CharList.js
+++ src/CharList.js
@@ -1,10 +1,15 @@
 import React from "react";
 import CharacterList from "./components/CharacterList";
 import SearchForm from "./components/SearchForm";
+import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const CharList = () => {
   return (
     <div>
+      <Link to="/">
+        <Header />
+      </Link>
       <SearchForm />
       <CharacterList />
     </div>
